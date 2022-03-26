65c1a7c69b8af89d31f9bc54fd0aea0503eff30c
Paula430 <paulakawalec8@gmail.com>
2022-03-26 12:56:41 +0100

    lab3

--- Lab3/SubroutineHierarchy.java
+++ Lab3/SubroutineHierarchy.java
@@ -4,11 +4,6 @@ import javax.swing.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
 
-/**
- * A panel that displays a two-dimensional animation that is drawn
- * using subroutines to implement hierarchical modeling.  There is a
- * checkbox that turns the animation on and off.
- */
 public class SubroutineHierarchy extends JPanel {
 
 	public static void main(String[] args) {
@@ -23,59 +18,56 @@ public class SubroutineHierarchy extends JPanel {
 
 	//-------------------------- Create the world and implement the animation --------------
 
-	private final static int WIDTH = 800;   // The preferred size for the drawing area.
+	private final static int WIDTH = 800;
 	private final static int HEIGHT = 600;
 
-	private final static double X_LEFT = -4;    // The xy limits for the coordinate system.
+	private final static double X_LEFT = -4;
 	private final static double X_RIGHT = 4;
 	private final static double Y_BOTTOM = -3;
 	private final static double Y_TOP = 3;
 
-	private final static Color BACKGROUND = Color.WHITE; // Initial background color for drawing.
+	private final static Color BACKGROUND = Color.WHITE;
 
-	private float pixelSize;  // The size of a pixel in drawing coordinates.
+	private float pixelSize;
 
-	private int frameNumber = 0;  // Current frame number, goes up by one in each frame.
+	private int frameNumber = 0;
 
 	// TODO:  Define any other necessary state variables.
 
-	/**
-	 * Responsible for drawing the entire scene.  The display is filled with the background
-	 * color before this method is called.
-	 */
+
 	private void drawWorld(Graphics2D g2) {
 
 		// TODO: Draw the content of the scene.
-		rotatingPolygon(g2);
-		rotatingPolygon(g2);
-		filledTriangle(g2,Color.blue);
-		filledRect(g2);
+		rotatingPolygon(g2,1,1,-105,-15);
+		rotatingPolygon(g2,1,1,55,-135);
+		filledTriangle(g2,Color.blue,1,1,0,0);
+		filledRect(g2,1,1,0,0);
 
 
+		rotatingPolygon(g2,0.7,0.7,-193,150);
+		rotatingPolygon(g2,0.7,0.7,-95,80);
+		filledTriangle(g2,new Color(128,0,128),0.8,0.8,-5,2);
+		filledRect(g2,0.6,0.6,-1,6);
 
+		rotatingPolygon(g2,0.4,0.4,147,27);
+		rotatingPolygon(g2,0.4,0.4,82,72);
+		filledTriangle(g2, new Color(0,128,0),0.5,0.5,3,2.5);
+		filledRect(g2,0.4,0.4,0.2,13.5);
 
 	}
 
 	private void updateFrame() {
 		frameNumber++;
 		// TODO: If other updates are needed for the next frame, do them here.
+
 	}
 
 
 	// TODO: Define methods for drawing objects in the scene.
 
-	private void rotatingRect(Graphics2D g2) { // (DELETE THIS EXAMPLE)
-		AffineTransform saveTransform = g2.getTransform();  // (It might be necessary to save/restore transform and color)
-		Color saveColor = g2.getColor();
-		g2.setColor(Color.RED);
-		g2.rotate(Math.toRadians(frameNumber * 0.75));
-		g2.scale(2, 2);
-		filledRect(g2);
-		g2.setColor(saveColor);
-		g2.setTransform(saveTransform);
-	}
 
-	private static void rotatingPolygon(Graphics2D g2) {
+
+	private void rotatingPolygon(Graphics2D g2, double scaleX, double scaleY,double translateX, double translateY) {
 		AffineTransform saveTransform = g2.getTransform();
 		int[] xpoints = new int[5];
 		int[] ypoints = new int[5];
@@ -95,7 +87,10 @@ public class SubroutineHierarchy extends JPanel {
 		g2.setStroke(new BasicStroke(4));
 		g2.rotate(Math.toRadians(18));
 		g2.scale(0.015, 0.015);
-
+		g2.translate(translateX, translateY);
+		g2.scale(0.3,0.3);
+		g2.scale(scaleX,scaleY);
+		g2.rotate( Math.toRadians( frameNumber*0.75 ));
 
 		for (int i = 1; i <= 5; i++) {
 			g2.drawLine(xpoints[i - 1], ypoints[i - 1], 0, 0);
@@ -119,11 +114,16 @@ public class SubroutineHierarchy extends JPanel {
 		g2.draw(new Rectangle2D.Double(-0.5,-0.5,1,1));
 	}
 
-	private static void filledRect(Graphics2D g2) { // Fills a square, size = 1, center = (0,0)
+	private static void filledRect(Graphics2D g2,double scaleX, double scaleY,double translateX, double translateY) { // Fills a red rectangle, size = 1, center = (0,0)
+		AffineTransform saveTransform = g2.getTransform();
 		g2.setColor(Color.RED);
 		g2.rotate(Math.toRadians(-18));
-		g2.scale(6,0.2);
+		g2.translate(0.35,-1.1);
+		g2.scale(3.1,0.2);
+		g2.translate(translateX, translateY);
+		g2.scale(scaleX,scaleY);
 		g2.fill(new Rectangle2D.Double(-0.5,-0.5,1,1));
+		g2.setTransform(saveTransform);
 
 	}
 
@@ -135,29 +135,31 @@ public class SubroutineHierarchy extends JPanel {
 		g2.draw(new Ellipse2D.Double(-0.5,-0.5,1,1));
 	}
 	
-	private static void filledTriangle(Graphics2D g2,Color color) { // width = 1, height = 1, center of base is at (0,0);
+	private static void filledTriangle(Graphics2D g2,Color color,double scaleX, double scaleY,double translateX, double translateY) { // width = 1, height = 1, center of base is at (0,0);
+
+		AffineTransform saveTransform = g2.getTransform();
 		Path2D path = new Path2D.Double();
 		g2.setColor(color);
 		path.moveTo(-0.5,0);
 		path.lineTo(0.5,0);
 		path.lineTo(0,1);
 		path.closePath();
-		g2.scale(0.5,1.4);
+		g2.scale(0.5,1.2);
+		g2.translate(0,-1);
+		g2.translate(-0.2,-1);
+		g2.translate(translateX, translateY);
+		g2.scale(scaleX,scaleY);
 		g2.fill(path);
+		g2.setTransform(saveTransform);
+
 	}
 
 
 
 	//--------------------------------- Implementation ------------------------------------
 
-	private JPanel display;  // The JPanel in which the scene is drawn.
+	private JPanel display;
 
-	/**
-	 * Constructor creates the scene graph data structure that represents the
-	 * scene that is to be drawn in this panel, by calling createWorld().
-	 * It also sets the preferred size of the panel to the constants WIDTH and HEIGHT.
-	 * And it creates a timer to drive the animation.
-	 */
 	public SubroutineHierarchy() {
 		display = new JPanel() {
 			protected void paintComponent(Graphics g) {
@@ -177,6 +179,7 @@ public class SubroutineHierarchy extends JPanel {
 				repaint();
 			}
 		});
+
 		final JCheckBox animationCheck = new JCheckBox("Run Animation");
 		animationCheck.addActionListener( new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
@@ -199,26 +202,6 @@ public class SubroutineHierarchy extends JPanel {
 		add(display,BorderLayout.CENTER);
 	}
 
-
-
-	/**
-	 * Applies a coordinate transform to a Graphics2D graphics context.  The upper left corner of 
-	 * the viewport where the graphics context draws is assumed to be (0,0).  The coordinate
-	 * transform will make a requested rectangle visible in the drawing area.  The requested
-	 * limits might be adjusted to preserve the aspect ratio.  (This method sets the global variable 
-	 * pixelSize to be equal to the size of one pixel in the transformed coordinate system.)
-	 * @param g2 The drawing context whose transform will be set.
-	 * @param xleft requested x-value at left of drawing area.
-	 * @param xright requested x-value at right of drawing area.
-	 * @param ytop requested y-value at top of drawing area.
-	 * @param ybottom requested y-value at bottom of drawing area; can be less than ytop, which will
-	 *     reverse the orientation of the y-axis to make the positive direction point upwards.
-	 * @param preserveAspect if preserveAspect is false, then the requested rectangle will exactly fill
-	 * the viewport; if it is true, then the limits will be expanded in one direction, horizontally or
-	 * vertically, to make the aspect ratio of the displayed rectangle match the aspect ratio of the
-	 * viewport.  Note that when preserveAspect is false, the units of measure in the horizontal and
-	 * vertical directions will be different.
-	 */
 	private void applyLimits(Graphics2D g2, double xleft, double xright, 
 			double ytop, double ybottom, boolean preserveAspect) {
 		int width = display.getWidth();   // The width of the drawing area, in pixels.
